933c3f2f1bed00083e3a7cdd1a6a4401d8e4b199
Yuri Benditovich <yuri.benditovich@daynix.com>
2021-03-31 18:15:48 +0300

    select available toolchain for build
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- make-123.sh
+++ make-123.sh
@@ -2,6 +2,11 @@
 filename=$3
 ext="${filename##*.}"
 rm -rf build
-CROSS_COMPILE=riscv64-linux-gnu- make PLATFORM=$1 FW_PAYLOAD_PATH=$2 -j8
+echo $PATH
+toolchain=riscv64-unknown-linux-gnu-
+which ${toolchain}gcc 2>/dev/null
+if [ $? -gt 0 ]; then toolchain=riscv64-linux-gnu- ; fi
+echo Using toolchain $toolchain
+CROSS_COMPILE=$toolchain make PLATFORM=$1 FW_PAYLOAD_PATH=$2 -j8
 mv build/platform/$1/firmware/fw_payload.$ext $3
 echo built for $1 with $2 to $3 
